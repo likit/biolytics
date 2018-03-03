cc5aa573c4a95faf9ad80c59529214198be2a929
Likit Preeyanon <preeyano@msu.edu>
2018-03-04 01:03:44 +0700

    added the sqlite3 database for storing app data.

--- app/server.py
+++ app/server.py
@@ -1,11 +1,24 @@
 # -*- coding: utf8 -*-
 import os
+import sqlite3
+from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, render_template, request
 from flask_uploads import UploadSet, configure_uploads
 
 app = Flask(__name__)
 
 rootdir = os.path.abspath(os.path.dirname(__file__))
+database_uri = 'sqlite:///{}'.format(os.path.join(rootdir, 'database.sqlite').replace('\\', '\\\\'))
+print(database_uri)
+app.config['SQLALCHEMY_DATABASE_URI'] = database_uri
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+import models
+
+db.create_all()
+
 app.config['UPLOADED_DATAFILES_DEST'] = os.path.join(rootdir, 'uploads')
 datafiles = UploadSet('datafiles', ('xlsx', 'csv', 'txt', 'tsv'))
 configure_uploads(app, (datafiles,))
